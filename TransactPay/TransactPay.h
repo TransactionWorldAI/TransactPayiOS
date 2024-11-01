70a5b14fd413d574a044fb3fb9acaa0e9954b213
James Anyanwu <geniusjames7@gmail.com>
2024-11-01 16:50:13 +0100

    Initial Commit

---
+++ TransactPay/TransactPay.h
@@ -0,0 +1,18 @@
+//
+//  TransactPay.h
+//  TransactPay
+//
+//  Created by James Anyanwu on 11/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TransactPay.
+FOUNDATION_EXPORT double TransactPayVersionNumber;
+
+//! Project version string for TransactPay.
+FOUNDATION_EXPORT const unsigned char TransactPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransactPay/PublicHeader.h>
+
+
